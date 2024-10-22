3447d51e82ea90fb3367a812936ddd4241f21714
FaizanTariq109 <f223714@cfd.nu.edu.pk>
2024-10-22 18:03:19 +0500

    AddToCart

---
+++ src/test/java/steps/AddToCartSteps.java
@@ -0,0 +1,53 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.AddToCartPage;
+import static org.junit.Assert.*;
+
+public class AddToCartSteps {
+    private WebDriver driver = null;
+    private AddToCartPage addCartPage;
+
+    @Given("the user is on the Daraz homepage")
+    public void the_user_is_on_the_daraz_homepage() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.daraz.pk/");
+        addCartPage = new AddToCartPage(driver);
+    }
+
+    @When("the user searches for a product {string}")
+    public void the_user_searches_for_a_product(String productName) {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(productName);
+        searchBox.submit();
+    }
+
+    @When("the user selects the first product from the search results")
+    public void the_user_selects_the_first_product() {
+        addCartPage.selectFirstProduct();
+    }
+
+    @When("the user adds the product to the cart")
+    public void the_user_adds_the_product_to_the_cart() {
+        addCartPage.addProductToCart();
+    }
+
+    @Then("the product should be added to the cart")
+    public void the_product_should_be_added_to_the_cart() {
+        assertTrue(addCartPage.isProductInCart());
+    }
+
+    @Then("the cart icon should show the number of items as {int}")
+    public void the_cart_icon_should_show_the_number_of_items_as(int expectedCount) {
+        assertEquals(expectedCount, addCartPage.getCartItemCount());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
