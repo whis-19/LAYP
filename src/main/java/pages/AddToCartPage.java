ef006c07df313bd826b05288de445d2ed2b0a0ed
Muhammad Abdullah <ma3472995@gmail.com>
2024-10-25 07:17:20 +0500

    finalized

--- src/main/java/pages/AddToCartPage.java
+++ src/main/java/pages/AddToCartPage.java
@@ -1,37 +1,5 @@
 package pages;
 
-<<<<<<< Updated upstream
-import org.openqa.selenium.*;
-
-public class AddToCartPage {
-    private WebDriver driver;
-
-    public AddToCartPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    public void selectFirstProduct() {
-        WebElement firstProduct = driver.findElement(By.cssSelector("div.s-result-item:first-child"));
-        firstProduct.click();
-    }
-
-    public void addProductToCart() {
-        WebElement addToCartButton = driver.findElement(By.id("add-to-cart"));
-        addToCartButton.click();
-    }
-
-    public boolean isProductInCart() {
-        driver.get("https://www.daraz.pk/cart");
-        return driver.findElements(By.cssSelector(".cart-item")).size() > 0;
-    }
-
-    public int getCartItemCount() {
-        WebElement cartIcon = driver.findElement(By.cssSelector(".cart-icon"));
-        String itemCountText = cartIcon.getText();
-        return itemCountText.isEmpty() ? 0 : Integer.parseInt(itemCountText);
-    }
-}
-=======
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -62,4 +30,3 @@ public class AddToCartPage {
         return cartCountElement.getText();
     }
 }
->>>>>>> Stashed changes
--- src/test/java/steps/AddToCartSteps.java
+++ src/test/java/steps/AddToCartSteps.java
@@ -1,51 +1,5 @@
 package steps;
 
-<<<<<<< Updated upstream
-import io.cucumber.java.After;
-import io.cucumber.java.en.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.AddToCartPage;
-import static org.junit.Assert.*;
-
-public class AddToCartSteps {
-    private WebDriver driver = null;
-    private AddToCartPage addCartPage;
-
-    @Given("the user is on the Daraz homepage")
-    public void the_user_is_on_the_daraz_homepage() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.daraz.pk/");
-        addCartPage = new AddToCartPage(driver);
-    }
-
-    @When("the user searches for a product {string}")
-    public void the_user_searches_for_a_product(String productName) {
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys(productName);
-        searchBox.submit();
-    }
-
-    @When("the user selects the first product from the search results")
-    public void the_user_selects_the_first_product() {
-        addCartPage.selectFirstProduct();
-    }
-
-    @When("the user adds the product to the cart")
-    public void the_user_adds_the_product_to_the_cart() {
-        addCartPage.addProductToCart();
-    }
-
-    @Then("the product should be added to the cart")
-    public void the_product_should_be_added_to_the_cart() {
-        assertTrue(addCartPage.isProductInCart());
-    }
-
-    @Then("the cart icon should show the number of items as {int}")
-    public void the_cart_icon_should_show_the_number_of_items_as(int expectedCount) {
-        assertEquals(expectedCount, addCartPage.getCartItemCount());
-=======
 import org.openqa.selenium.WebDriver;
 import config.DriverFactory;
 import io.cucumber.java.After;
@@ -89,19 +43,14 @@ public class AddToCartSteps {
 
         int expectedCount = 1;
         Assert.assertEquals("The cart should indicate items are added", expectedCount, Integer.parseInt(cartItemCount));
->>>>>>> Stashed changes
     }
 
     @After
     public void tearDown() {
-<<<<<<< Updated upstream
-        driver.quit();
-=======
         try {
             DriverFactory.quitDriver();
         } catch (Exception e) {
             System.err.println("Error quitting driver: " + e.getMessage());
         }
->>>>>>> Stashed changes
     }
 }
